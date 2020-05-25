82ecf1f3015f9e0f59575876142c53b66f92483b
Jack <907739769@qq.com>
2020-05-25 08:56:54 +0800

    2020-05-25 08:56版本

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,97 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    <button v-on:click="loadMore">click</button>
+    <ul v-for="(imageList,index) in imageLists" :key="index">
+      <li v-for="(item,id) in imageList" :key="id">
+        <a v-bind:href="item.url">
+          <img alt="baby" class="simg" mode="aspectFit" v-bind:src="item.thumbUrl"/>
+        </a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import axios from 'axios'
+
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String,
+      // url: String
+    },
+    data() {
+      return {
+        // title: "静态数据",
+        // url: "e",
+        // thumbUrl:"e"
+        imageList:[],
+        imageLists:[],
+        imgListIndex:0,
+        pageNum:1,
+        pageSize:24,
+      }
+    },
+    //在这里调用ajax请求方法
+    created(){
+      this.convert();
+    },
+    methods: {
+      convert: function () {
+        axios.get("http://127.0.0.1:8101/image/get",{
+          params:{
+            "pageNum":this.pageNum,
+            "pageSize":this.pageSize,
+          }
+        }).then(res => {
+          // this.thumbUrl = res.data.records[0].thumbUrl;
+          // this.url = res.data.records[0].url;
+          this.imageLists.push(res.data.records);
+          //this.imageLists[this.imgListIndex]=res.data.records;
+          //console.log(this.imageLists);
+        })
+        this.pageNum++;
+      },
+      loadMore: function () {
+        axios.get("http://127.0.0.1:8101/image/get",{
+          params:{
+            "pageNum":this.pageNum,
+            "pageSize":this.pageSize,
+          }
+        }).then(res => {
+          // this.thumbUrl = res.data.records[0].thumbUrl;
+          // this.url = res.data.records[0].url;
+          this.imageLists.push(res.data.records);
+          console.log(this.imageList);
+          //this.imageLists[this.imgListIndex]=res.data.records;
+          //console.log(this.imageLists);
+        })
+        this.pageNum++;
+      }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 0;
+  }
+  a {
+    color: #42b983;
+    margin: 0.3125rem;
+  }
+  .simg{
+    width: auto;
+    /* height: 20rem */
+  }
 </style>
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,13 +1,27 @@
 <template>
-    
+    <h2 class="red">test...{{message}}</h2>
 </template>
 
 <script>
     export default {
-        name: ""
+        name: "test",
+        props:{
+            msg:{
+                type:String,
+                default:"test msg"
+            }
+        },
+        methods:{
+
+        },
+        data(){
+            return{
+
+            }
+        },
     }
 </script>
 
 <style scoped>
-
+.red{color: red}
 </style>
